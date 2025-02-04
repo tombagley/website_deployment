2f32b08fccce5d2125041d797a137c13dbd29ab6
tombagley <thomaswilliambagley@gmail.com>
2025-02-04 16:30:27 -0600

    Update and rename vite.config.cjs to vite.config.js

--- client/vite.config.cjs
+++
@@ -1,9 +0,0 @@
-// vite.config.cjs
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
-
-// https://vite.dev/config/
-module.exports = defineConfig({
-  root: 'dist',
-  plugins: [react()],
-});
